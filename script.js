c9dec6675ac6a6dd0f5f3103727645dd5935365d
ElKekso <159456154+ElKekso@users.noreply.github.com>
2025-09-25 12:29:30 +0200

    Add Icons and styling

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ function createProjectBlock(heading_text, text) {
     icon_container = document.createElement("div");
     //replace icons with actual fontawesome icons
     icons = [];
-    icons.push(document.createElement("i"));
-    icons.push(document.createElement("i"));
-    icons.push(document.createElement("i"));
+    icons.push(getIcon("fa-regular", "fa-star", "fa-xl"));
+    icons.push(getIcon("fa-regular", "fa-eye", "fa-xl"));
+    icons.push(getIcon("fa-solid", "fa-share-from-square", "fa-xl"));
+
+    
 
     icons.forEach(element => {
         icon_container.appendChild(element);
@@ -24,6 +26,16 @@ function createProjectBlock(heading_text, text) {
     article.appendChild(icon_container);
 
     return article;
+}
+
+function getIcon(source, icon_name, sizing) {
+    icon = document.createElement("i");
+
+    icon.classList.add(source);
+    icon.classList.add(icon_name);
+    icon.classList.add(sizing);
+    
+    return icon;
 }
 
 projects_list = document.querySelector(".projects");
